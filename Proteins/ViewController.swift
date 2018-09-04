2ced13706f93eb5c806e4b0e56497acb201af9d3
Luis NIETO MUNOZ <lnieto-m@e2r7p5.42.fr>
2018-09-04 18:01:44 +0200

    first push

---
+++ Proteins/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Proteins
+//
+//  Created by Luis NIETO MUNOZ on 9/4/18.
+//  Copyright © 2018 Luis NIETO MUNOZ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
