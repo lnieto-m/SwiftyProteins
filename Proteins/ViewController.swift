8968de7c0a3b14d72c297e81020ae1c2ce9ae982
Luis NIETO MUNOZ <lnieto-m@e2r3p6.42.fr>
2018-09-06 14:12:14 +0200

    name list

--- Proteins/ViewController.swift
+++ Proteins/ViewController.swift
@@ -11,7 +11,8 @@ import UIKit
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate {
  
     @IBOutlet weak var LingandTableView: UITableView!
-    var filteredData: [String]!
+    var ligands: [String] = []
+    var filteredData: [String] = []
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return filteredData.count
@@ -24,7 +25,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     }
     
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-        filteredData = searchText.isEmpty ? LingandList.List : LingandList.List.filter {(item: String) -> Bool in
+        filteredData = searchText.isEmpty ? ligands : ligands.filter {(item: String) -> Bool in
             return item.range(of: searchText, options: .caseInsensitive, range: nil, locale: nil) != nil
         }
         LingandTableView.reloadData()
@@ -39,17 +40,18 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
             do {
                 let data = try String(contentsOf: filePath!)
                 print(data)
+                let splittedData = data.split(separator: "\n")
+                for value in splittedData {
+                    ligands.append(String(value))
+                }
             } catch {
                 print("not found")
             }
-            //            print(bundle.isLoaded)
-//            bundle.load()
-//            print(bundle.isLoaded)
         } else {
             print("not found")
         }
         
-        filteredData =  LingandList.List
+        filteredData =  ligands
         // Do any additional setup after loading the view, typically from a nib.
     }
 
